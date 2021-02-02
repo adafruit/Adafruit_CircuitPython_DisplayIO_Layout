3d6d4430520ef6494bd55d6cb074ea32fa14d28b
Kevin Matocha <kmatocha@icloud.com>
2021-02-02 09:41:34 -0600

    Ran black, somehow failed for widget_label.py

--- widgets/switch_round_horizontal.py
+++ widgets/switch_round_horizontal.py
@@ -1,4 +1,3 @@
-
 # The MIT License (MIT)
 #
 # Copyright (c) 2021 Kevin Matocha (kmatch98)
@@ -80,7 +79,6 @@ class SwitchRoundHorizontal(Widget, Control):
     :param float animation_time: time for the switching animation, in seconds
      a value of 0.2 is a good starting point"""
 
-
     # This Switch has multiple class inheritances.
     # It is a subclass of Group->Widget and a sublcass of Control.
 
@@ -91,36 +89,35 @@ class SwitchRoundHorizontal(Widget, Control):
         anchor_point=None,
         anchored_position=None,
         fill_color_off=(66, 44, 66),
-        fill_color_on=(0,100,0),
-        outline_color_off=(30,30,30),
-        outline_color_on=(0,60,0),
-        background_color_off=(255,255,255),
-        background_color_on=(0,60,0),
-        background_outline_color_off=None, # default to background_color_off
-        background_outline_color_on=None, # default to background_color_on
+        fill_color_on=(0, 100, 0),
+        outline_color_off=(30, 30, 30),
+        outline_color_on=(0, 60, 0),
+        background_color_off=(255, 255, 255),
+        background_color_on=(0, 60, 0),
+        background_outline_color_off=None,  # default to background_color_off
+        background_outline_color_on=None,  # default to background_color_on
         switch_stroke=2,
-        text_stroke=None, # default to switch_stroke
+        text_stroke=None,  # default to switch_stroke
         display_button_text=True,
-        animation_time=0.2, # animation duration (in seconds)
+        animation_time=0.2,  # animation duration (in seconds)
         **kwargs,
-        ):
+    ):
 
-
-        #initialize the Widget superclass (x, y, scale)
+        # initialize the Widget superclass (x, y, scale)
         super().__init__(**kwargs, max_size=4)
-            # Define how many graphical elements will be in this group
-            # using "max_size=XX"
-            #
-            # Group elements for SwitchRoundHorizontal:
-            #  1. switch_roundrect: The switch background
-            #  2. switch_circle: The switch button
-            #  3. Optional - widget label
-            #  4. Optional - text_0 or text_1: The 0/1 text on the switch button
+        # Define how many graphical elements will be in this group
+        # using "max_size=XX"
+        #
+        # Group elements for SwitchRoundHorizontal:
+        #  1. switch_roundrect: The switch background
+        #  2. switch_circle: The switch button
+        #  3. Optional - widget label
+        #  4. Optional - text_0 or text_1: The 0/1 text on the switch button
 
         # initialize the Control superclass
         super(Control, self).__init__()
 
-        self._radius = self.height//2
+        self._radius = self.height // 2
         switch_x = self._radius
         switch_y = self._radius
 
@@ -143,13 +140,15 @@ class SwitchRoundHorizontal(Widget, Control):
         self._background_outline_color_off = background_outline_color_off
         self._background_outline_color_on = background_outline_color_on
 
-        self._switch_stroke=switch_stroke
+        self._switch_stroke = switch_stroke
 
         if text_stroke is None:
-            text_stroke = switch_stroke # width of text lines
+            text_stroke = switch_stroke  # width of text lines
         self._text_stroke = text_stroke
 
-        self._display_button_text = display_button_text # state variable whether text (0/1) is displayed
+        self._display_button_text = (
+            display_button_text  # state variable whether text (0/1) is displayed
+        )
 
         self._touch_padding = touch_padding
 
@@ -157,58 +156,74 @@ class SwitchRoundHorizontal(Widget, Control):
 
         self._value = value
 
-        self._text_0_on = not value # controls which text value is displayed (0 or 1)
+        self._text_0_on = not value  # controls which text value is displayed (0 or 1)
 
         self._anchor_point = anchor_point
         self._anchored_position = anchored_position
 
         # initialize the display elements
 
-        self._switch_circle = Circle(x0=switch_x, y0=switch_y,
-                           r=self._radius,
-                           fill=self._fill_color_off,
-                           outline=self._outline_color_off,
-                           stroke=self._switch_stroke)
+        self._switch_circle = Circle(
+            x0=switch_x,
+            y0=switch_y,
+            r=self._radius,
+            fill=self._fill_color_off,
+            outline=self._outline_color_off,
+            stroke=self._switch_stroke,
+        )
 
-        self._switch_roundrect = RoundRect( x=switch_x-self._radius,
-                                            y=switch_y-self._radius,
-                                            r=self._radius,
-                                            width=self._width, height=2*self._radius+1,
-                                            fill=self._background_color_off,
-                                            outline=self._background_outline_color_off,
-                                            stroke=self._switch_stroke)
+        self._switch_roundrect = RoundRect(
+            x=switch_x - self._radius,
+            y=switch_y - self._radius,
+            r=self._radius,
+            width=self._width,
+            height=2 * self._radius + 1,
+            fill=self._background_color_off,
+            outline=self._background_outline_color_off,
+            stroke=self._switch_stroke,
+        )
 
         # bounding_box defines the "local" x and y.
         # Must be offset by self.x and self.y to get the raw display coordinates
-        self._bounding_box = [self._switch_circle.x,
-                              self._switch_circle.y,
-                              self._width,
-                              2*self._radius+1]
+        self._bounding_box = [
+            self._switch_circle.x,
+            self._switch_circle.y,
+            self._width,
+            2 * self._radius + 1,
+        ]
 
-        self.touch_boundary = [self._bounding_box[0]-self._touch_padding,
-                      self._bounding_box[1]-self._touch_padding,
-                      self._bounding_box[2]+2*self._touch_padding,
-                      self._bounding_box[3]+2*self._touch_padding]
+        self.touch_boundary = [
+            self._bounding_box[0] - self._touch_padding,
+            self._bounding_box[1] - self._touch_padding,
+            self._bounding_box[2] + 2 * self._touch_padding,
+            self._bounding_box[3] + 2 * self._touch_padding,
+        ]
 
         # The "0" text circle
-        self._text_0 = Circle(x0=switch_x, y0=switch_y,
-                           r=self._radius//2,
-                           fill=self._fill_color_off,
-                           outline=self._outline_color_off,
-                           stroke=self._text_stroke)
+        self._text_0 = Circle(
+            x0=switch_x,
+            y0=switch_y,
+            r=self._radius // 2,
+            fill=self._fill_color_off,
+            outline=self._outline_color_off,
+            stroke=self._text_stroke,
+        )
 
         # The "1" text rectangle
-        self._text_1 = Rect(x=switch_x-self._switch_stroke+1, y=switch_y-self._radius//2,
-                           height=self._radius,
-                           width=self._text_stroke,
-                           fill=self._fill_color_off,
-                           outline=self._outline_color_off,
-                           stroke=self._text_stroke)
+        self._text_1 = Rect(
+            x=switch_x - self._switch_stroke + 1,
+            y=switch_y - self._radius // 2,
+            height=self._radius,
+            width=self._text_stroke,
+            fill=self._fill_color_off,
+            outline=self._outline_color_off,
+            stroke=self._text_stroke,
+        )
 
         # Store initial positions of the moving parts
-        self._switch_initial_x=self._switch_circle.x
-        self._text_0_initial_x=self._text_0.x
-        self._text_1_initial_x=self._text_1.x
+        self._switch_initial_x = self._switch_circle.x
+        self._text_0_initial_x = self._text_0.x
+        self._text_1_initial_x = self._text_1.x
 
         # Set the initial switch position based on the starting value
         if value:
@@ -216,24 +231,21 @@ class SwitchRoundHorizontal(Widget, Control):
         else:
             self._draw_position(0)
 
-
-
         # Add the display elements to the self group
         self.append(self._switch_roundrect)
         self.append(self._switch_circle)
 
         # Create the widget label
-        self.widget_label=None
-        if (self.name != "") :
-            font=terminalio.FONT
-            self.widget_label=WidgetLabel(font,
-                                          self,
-                                          anchor_point=(1,0.5),
-                                          anchor_point_on_widget=(-0.05, 0.5))
+        self.widget_label = None
+        if self.name != "":
+            font = terminalio.FONT
+            self.widget_label = WidgetLabel(
+                font, self, anchor_point=(1, 0.5), anchor_point_on_widget=(-0.05, 0.5)
+            )
 
         # If display_button_text is True, append the correct text element (0 or 1)
         if display_button_text:
-            if (self._text_0_on):
+            if self._text_0_on:
                 self.append(self._text_0)
             else:
                 self.append(self._text_1)
@@ -241,77 +253,106 @@ class SwitchRoundHorizontal(Widget, Control):
         # update the position, if required
         self._update_position
 
-
     def _draw_position(self, position):
 
         # Draw the position of the slider.
         # The position is a float between 0 and 1 (0= off, 1= on).
 
         # To deal with any rounding errors
-        if position <= 0: # left-end position
+        if position <= 0:  # left-end position
             self._switch_circle.x = self._switch_initial_x
-            self._text_0.x=self._text_0_initial_x
-            self._text_1.x=self._text_1_initial_x
-        elif position >= 1: # right-end position
-            self._switch_circle.x = self._switch_initial_x + self._width-2*self._radius-1
-            self._text_0.x = self._text_0_initial_x + self._width-2*self._radius-1
-            self._text_1.x = self._text_1_initial_x + self._width-2*self._radius-1
-        else: # somewhere in the middle
-            self._switch_circle.x = self._switch_initial_x + int((self._width - (2 * self._radius) - 1)*position)
-            self._text_0.x = self._text_0_initial_x + int((self._width - (2 * self._radius) - 1)*position)
-            self._text_1.x = self._text_1_initial_x + int((self._width - (2 * self._radius) - 1)*position)
+            self._text_0.x = self._text_0_initial_x
+            self._text_1.x = self._text_1_initial_x
+        elif position >= 1:  # right-end position
+            self._switch_circle.x = (
+                self._switch_initial_x + self._width - 2 * self._radius - 1
+            )
+            self._text_0.x = self._text_0_initial_x + self._width - 2 * self._radius - 1
+            self._text_1.x = self._text_1_initial_x + self._width - 2 * self._radius - 1
+        else:  # somewhere in the middle
+            self._switch_circle.x = self._switch_initial_x + int(
+                (self._width - (2 * self._radius) - 1) * position
+            )
+            self._text_0.x = self._text_0_initial_x + int(
+                (self._width - (2 * self._radius) - 1) * position
+            )
+            self._text_1.x = self._text_1_initial_x + int(
+                (self._width - (2 * self._radius) - 1) * position
+            )
 
         # Set the color to the correct fade
-        self._switch_circle.fill = _color_fade(self._fill_color_off, self._fill_color_on, position)
-        self._switch_circle.outline = _color_fade(self._outline_color_off, self._outline_color_on, position)
+        self._switch_circle.fill = _color_fade(
+            self._fill_color_off, self._fill_color_on, position
+        )
+        self._switch_circle.outline = _color_fade(
+            self._outline_color_off, self._outline_color_on, position
+        )
 
-        self._switch_roundrect.fill = _color_fade(self._background_color_off, self._background_color_on, position)
-        self._switch_roundrect.outline = _color_fade(self._background_outline_color_off, self._background_outline_color_on, position)
+        self._switch_roundrect.fill = _color_fade(
+            self._background_color_off, self._background_color_on, position
+        )
+        self._switch_roundrect.outline = _color_fade(
+            self._background_outline_color_off,
+            self._background_outline_color_on,
+            position,
+        )
 
         self._text_0.fill = self._switch_circle.fill
         self._text_1.fill = self._switch_circle.fill
         self._text_0.outline = self._switch_circle.outline
         self._text_1.outline = self._switch_circle.outline
 
-        if (self._display_button_text and position > 0.5 and self._text_0_on):
+        if self._display_button_text and position > 0.5 and self._text_0_on:
             self.pop()
             self.append(self._text_1)
             self._text_0_on = False
 
-        elif (self._display_button_text and position < 0.5 and not self._text_0_on):
+        elif self._display_button_text and position < 0.5 and not self._text_0_on:
             self.pop()
             self.append(self._text_0)
             self._text_0_on = True
 
     def selected(self, touch_point):
-            # requires passing display to allow auto_refresh off when redrawing
-            # touch_point is adjusted for group's x,y position before sending to super()
+        # requires passing display to allow auto_refresh off when redrawing
+        # touch_point is adjusted for group's x,y position before sending to super()
 
-        start_time=time.monotonic()
+        start_time = time.monotonic()
 
         while True:
             if self._value:
-                position = 1 - (time.monotonic() - start_time)/self._animation_time # fraction from 0 to 1
+                position = (
+                    1 - (time.monotonic() - start_time) / self._animation_time
+                )  # fraction from 0 to 1
             else:
-                position = (time.monotonic() - start_time)/self._animation_time # fraction from 0 to 1
+                position = (
+                    time.monotonic() - start_time
+                ) / self._animation_time  # fraction from 0 to 1
 
-            self._draw_position(position) # update the switch position
+            self._draw_position(position)  # update the switch position
 
-            if ((position >= 1) and not self._value): # ensures that the final position is drawn
-                self._value=True
+            if (
+                position >= 1
+            ) and not self._value:  # ensures that the final position is drawn
+                self._value = True
                 break
-            if ((position <= 0) and self._value): # ensures that the final position is drawn
-                self._value=False
+            if (
+                position <= 0
+            ) and self._value:  # ensures that the final position is drawn
+                self._value = False
                 break
 
-            touch_x = touch_point[0] - self.x # adjust touch position for the local position
+            touch_x = (
+                touch_point[0] - self.x
+            )  # adjust touch position for the local position
             touch_y = touch_point[1] - self.y
 
             super().selected((touch_x, touch_y, 0))
 
-    def contains(self, touch_point): # overrides, then calls Control.contains(x,y)
+    def contains(self, touch_point):  # overrides, then calls Control.contains(x,y)
         """Returns True if the touch_point is within the widget's touch_boundary."""
-        touch_x = touch_point[0] - self.x # adjust touch position for the local position
+        touch_x = (
+            touch_point[0] - self.x
+        )  # adjust touch position for the local position
         touch_y = touch_point[1] - self.y
 
         return super().contains((touch_x, touch_y, 0))
@@ -323,12 +364,13 @@ class SwitchRoundHorizontal(Widget, Control):
 
     @value.setter
     def value(self, new_value):
-        if (new_value != self._value):
-            fake_touch_point=[0,0,0] # send an arbitrary touch_point
+        if new_value != self._value:
+            fake_touch_point = [0, 0, 0]  # send an arbitrary touch_point
             self.selected(fake_touch_point)
 
 
 ######  color support functions  ######
+
 
 def _color_to_tuple(value):
     """Converts a color from a 24-bit integer to a tuple.
@@ -346,6 +388,7 @@ def _color_to_tuple(value):
     else:
         raise ValueError("Color must be a tuple or 24-bit integer value.")
 
+
 def _color_fade(start_color, end_color, fraction):
     """Linear extrapolation of a color between two RGB colors (tuple or 24-bit integer).
     : param start_color: starting color
@@ -354,15 +397,16 @@ def _color_fade(start_color, end_color, fraction):
     fraction of interpolation between start_color and end_color.
     """
 
-    start_color =_color_to_tuple(start_color)
+    start_color = _color_to_tuple(start_color)
     end_color = _color_to_tuple(end_color)
     if fraction >= 1:
         return end_color
     if fraction <= 0:
         return start_color
     else:
-        faded_color = [0,0,0]
+        faded_color = [0, 0, 0]
         for i in range(3):
-            faded_color[i] = start_color[i] - int((start_color[i]-end_color[i])* fraction)
+            faded_color[i] = start_color[i] - int(
+                (start_color[i] - end_color[i]) * fraction
+            )
         return faded_color
-
