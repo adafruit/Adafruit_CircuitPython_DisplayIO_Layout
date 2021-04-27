4317f4b0fcd04cae6600ff5be299846426b27470
jposada202020 <jquintana202020@gmail.com>
2021-04-27 16:35:41 -0400

    showing_parameters

--- docs/conf.py
+++ docs/conf.py
@@ -39,9 +39,6 @@ intersphinx_mapping = {
     "CircuitPython": ("https://circuitpython.readthedocs.io/en/latest/", None),
 }
 
-# Show the docstring from both the class and its __init__() method.
-autoclass_content = "init"
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
