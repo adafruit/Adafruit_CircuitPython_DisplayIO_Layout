779fc7a1e48588980531fff9de6378d1413520f9
s-light <git@s-light.eu>
2021-12-19 19:42:42 +0100

    cleanup example

--- examples/displayio_layout_cartesian_lineplot.py
+++ examples/displayio_layout_cartesian_lineplot.py
@@ -20,12 +20,12 @@ display = board.DISPLAY
 # Create a Cartesian widget
 # https://circuitpython.readthedocs.io/projects/displayio-layout/en/latest/api.html#module-adafruit_displayio_layout.widgets.cartesian
 my_plane = Cartesian(
-    x=20,  # x position for the plane
-    y=0,  # y plane position
-    width=130,  # display width
+    x=15,  # x position for the plane
+    y=2,  # y plane position
+    width=140,  # display width
     height=105,  # display height
-    xrange=(0, 100),  # x range
-    yrange=(0, 100),  # y range
+    xrange=(0, 10),  # x range
+    yrange=(0, 10),  # y range
 )
 
 my_group = displayio.Group()
@@ -33,20 +33,31 @@ my_group.append(my_plane)
 display.show(my_group)  # add high level Group to the display
 
 data = [
-    (0, 0),
+    # (0, 0),  # we do this point manually - so we have no wait...
+    (1, 1),
+    (2, 1),
+    (2, 2),
+    (3, 3),
+    (4, 3),
+    (4, 4),
+    (5, 5),
+    (6, 5),
+    (6, 6),
+    (7, 7),
+    (8, 7),
+    (8, 8),
+    (9, 9),
+    (10, 9),
     (10, 10),
-    (30, 10),
-    (50, 50),
-    (70, 30),
-    (90, 80),
-    (95, 80),
-    (100, 100),
 ]
 
 print("examples/displayio_layout_cartesian_lineplot.py")
+
+# first point without a wait.
+my_plane.update_line(0, 0)
 for x, y in data:
     my_plane.update_line(x, y)
-    time.sleep(1)
+    time.sleep(0.5)
 
 while True:
     pass
