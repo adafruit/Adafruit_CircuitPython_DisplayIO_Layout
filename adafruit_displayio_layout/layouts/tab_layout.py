b55c3e33480e3d37582a4b8dd00e42531f2568d3
foamyguy <foamyguy@gmail.com>
2022-05-06 18:10:14 -0500

    working on tab_layout and simpletest example

--- adafruit_displayio_layout/layouts/tab_layout.py
+++ adafruit_displayio_layout/layouts/tab_layout.py
@@ -1,13 +1,35 @@
+# SPDX-FileCopyrightText: 2022 Tim Cocks
+#
+# SPDX-License-Identifier: MIT
 
+"""
+`tab_layout`
+================================================================================
+
+A layout that organizes pages into tabs.
+
+
+* Author(s): Tim Cocks
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+"""
 try:
-    from typing import Optional, Union
+    from typing import Optional, Union, Tuple
     from fontio import BuiltinFont
     from adafruit_bitmap_font.bdf import BDF
     from adafruit_bitmap_font.pcf import PCF
 except ImportError:
     pass
 
-import board
 import terminalio
 import displayio
 import adafruit_imageload
@@ -15,21 +37,55 @@ from adafruit_display_text.bitmap_label import Label
 from adafruit_imageload.tilegrid_inflator import inflate_tilegrid
 from adafruit_displayio_layout.layouts.page_layout import PageLayout
 
-class TabLayout(displayio.Group):
-    def __init__(self,
-                 x: int = 0, y: int = 0,
-                 display: displayio.Display = board.DISPLAY,
-                 tab_text_scale: int = 1,
-                 custom_font: Optional[Union[BuiltinFont, BDF, PCF]] = terminalio.FONT,
-                 inactive_tab_spritesheet: Optional[str] = None,
-                 active_tab_spritesheet: Optional[str] = None,
-                 active_tab_text_color: Optional[int, tuple[int, int, int]] = 0x999999,
-                 inactive_tab_text_color: Optional[int, tuple[int, int, int]] = 0xfffff,
-                 inactive_tab_transparent_indexes: Optional[Union[int, tuple[int, int]]] = None,
-                 active_tab_transparent_indexes: Optional[Union[int, tuple[int, int]]] = None,
-                 tab_count: int = None,
-                 ):
 
+class TabLayout(displayio.Group):
+    """
+    A layout that organizes children into a grid table structure.
+
+    :param int x: x location the layout should be placed. Pixel coordinates.
+    :param int y: y location the layout should be placed. Pixel coordinates.
+    :param displayio.Display display: The Display object to show the tab layout on.
+    :param int tab_text_scale: Size of the text shown in the tabs.
+      Whole numbers 1 and greater are valid
+    :param Optional[Union[BuiltinFont, BDF, PCF]] custom_font: A pre-loaded font object to use
+      for the tab labels
+    :param str inactive_tab_spritesheet: Filepath of the spritesheet to show for inactive tabs.
+    :param str active_tab_spritesheet: Filepath of the spritesheet to show for the active tab.
+    :param Optional[int, tuple[int, int, int]] active_tab_text_color: Hex or tuple color to use
+      for the active tab label
+    :param Optional[int, tuple[int, int, int]] inactive_tab_text_color: Hex or tuple color to
+      use for inactive tab labels
+    :param Optional[Union[int, tuple[int, int]]] inactive_tab_transparent_indexes: single index
+      or tuple of multiple indexes to be made transparent in the inactive tab sprite palette.
+    :param Optional[Union[int, tuple[int, int]]] active_tab_transparent_indexes: single index
+      or tuple of multiple indexes to be made transparent in the active tab sprite palette.
+    :param int tab_count: How many tabs to draw in the layout. Positive whole numbers are valid.
+    """
+
+    # pylint: disable=too-many-instance-attributes, too-many-arguments, invalid-name, too-many-branches
+
+    def __init__(
+        self,
+        x: int = 0,
+        y: int = 0,
+        display: Optional[displayio.Display] = None,
+        tab_text_scale: int = 1,
+        custom_font: Optional[Union[BuiltinFont, BDF, PCF]] = terminalio.FONT,
+        inactive_tab_spritesheet: Optional[str] = None,
+        active_tab_spritesheet: Optional[str] = None,
+        active_tab_text_color: Optional[Union[int, Tuple[int, int, int]]] = 0x999999,
+        inactive_tab_text_color: Optional[Union[int, Tuple[int, int, int]]] = 0xFFFFF,
+        inactive_tab_transparent_indexes: Optional[Union[int, Tuple[int, int]]] = None,
+        active_tab_transparent_indexes: Optional[Union[int, Tuple[int, int]]] = None,
+        tab_count: int = None,
+    ):
+
+        if display is None:
+            # pylint: disable=import-outside-toplevel
+            import board
+
+            if hasattr(board, "DISPLAY"):
+                display = board.DISPLAY
         if inactive_tab_spritesheet is None:
             raise AttributeError("Must pass active_tab_spritesheet")
         if active_tab_spritesheet is None:
@@ -39,8 +95,12 @@ class TabLayout(displayio.Group):
 
         super().__init__(x=x, y=y)
         self.tab_count = tab_count
-        self._active_bmp, self._active_palette = adafruit_imageload.load(active_tab_spritesheet)
-        self._inactive_bmp, self._inactive_palette = adafruit_imageload.load(inactive_tab_spritesheet)
+        self._active_bmp, self._active_palette = adafruit_imageload.load(
+            active_tab_spritesheet
+        )
+        self._inactive_bmp, self._inactive_palette = adafruit_imageload.load(
+            inactive_tab_spritesheet
+        )
 
         if isinstance(active_tab_transparent_indexes, int):
             self._active_palette.make_transparent(active_tab_transparent_indexes)
@@ -56,7 +116,9 @@ class TabLayout(displayio.Group):
             for index in inactive_tab_transparent_indexes:
                 self._inactive_palette.make_transparent(index)
         else:
-            raise AttributeError("inactive_tab_transparent_indexes must be int or tuple")
+            raise AttributeError(
+                "inactive_tab_transparent_indexes must be int or tuple"
+            )
 
         self.tab_height = self._active_bmp.height
         self.display = display
@@ -72,25 +134,35 @@ class TabLayout(displayio.Group):
         self.append(self.page_layout)
 
     def _draw_tabs(self):
-        for i, page_dict in enumerate(self.page_layout._page_content_list):
+        for i, page_dict in enumerate(self.page_layout.page_content_list):
             if i not in self.tab_dict:
                 print(f"creating tab {i}")
                 _new_tab_group = displayio.Group()
-                _tab_tilegrid = inflate_tilegrid(bmp_obj=self._inactive_bmp, bmp_palette=self._inactive_palette,
-                                                 target_size=((self.display.width // self.tab_count) // (
-                                                         self._active_bmp.width // 3), 3))
+                _tab_tilegrid = inflate_tilegrid(
+                    bmp_obj=self._inactive_bmp,
+                    bmp_palette=self._inactive_palette,
+                    target_size=(
+                        (self.display.width // self.tab_count)
+                        // (self._active_bmp.width // 3),
+                        3,
+                    ),
+                )
 
                 _tab_tilegrid.x = (self.display.width // self.tab_count) * i
                 _new_tab_group.append(_tab_tilegrid)
 
-                _tab_label = Label(self.custom_font, text=page_dict["page_name"],
-                                   color=self.inactive_tab_text_color, scale=self.tab_text_scale)
+                _tab_label = Label(
+                    self.custom_font,
+                    text=page_dict["page_name"],
+                    color=self.inactive_tab_text_color,
+                    scale=self.tab_text_scale,
+                )
 
                 _tab_label.anchor_point = (0.5, 0.5)
                 _tab_label.anchored_position = (
-                    _tab_tilegrid.x +
-                    ((_tab_tilegrid.width * _tab_tilegrid.tile_width) // 2),
-                    (_tab_tilegrid.height * _tab_tilegrid.tile_height) // 2
+                    _tab_tilegrid.x
+                    + ((_tab_tilegrid.width * _tab_tilegrid.tile_width) // 2),
+                    (_tab_tilegrid.height * _tab_tilegrid.tile_height) // 2,
                 )
                 _new_tab_group.append(_tab_label)
 
@@ -113,9 +185,14 @@ class TabLayout(displayio.Group):
                 self.tab_group[i][1].color = self.inactive_tab_text_color
 
     def add_content(self, tab_content, tab_name):
+        """Add a child to the tab layout.
+
+        :param tab_content: the content for the tab typically a Group
+        :param tab_name: the name of this tab, will be shown inside the tab
+
+        :return: None"""
         self.page_layout.add_content(tab_content, tab_name)
         self._draw_tabs()
-
 
     def show_page(self, page_name=None, page_index=None):
         """
@@ -129,7 +206,6 @@ class TabLayout(displayio.Group):
         self.page_layout.show_page(page_name=page_name, page_index=page_index)
         self._update_active_tab()
 
-
     @property
     def showing_page_index(self):
         """
@@ -140,7 +216,8 @@ class TabLayout(displayio.Group):
 
     @showing_page_index.setter
     def showing_page_index(self, new_index):
-        self.show_page(page_index=new_index)
+        if self.showing_page_index != new_index:
+            self.show_page(page_index=new_index)
 
     @property
     def showing_page_name(self):
@@ -180,3 +257,21 @@ class TabLayout(displayio.Group):
         """
         self.page_layout.previous_page(loop=loop)
         self._update_active_tab()
+
+    def handle_touch_events(self, touch_event):
+        """
+        Check if the touch event is on the tabs and if so change to the touched tab.
+
+        :param tuple touch_event: tuple containing x and y coordinates of the
+          touch event in indexes 0 and 1.
+        :return: None
+        """
+
+        if touch_event:
+            if 0 <= touch_event[1] <= self.tab_height:
+
+                touched_tab_index = touch_event[0] // (
+                    self.display.width // self.tab_count
+                )
+                print(f"{touch_event[0]} - {touched_tab_index}")
+                self.showing_page_index = touched_tab_index
