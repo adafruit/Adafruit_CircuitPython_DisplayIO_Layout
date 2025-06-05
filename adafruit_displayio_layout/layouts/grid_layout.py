74411706db2f863ced498925409b4060a99492c2
Shubham Patel <165564832+shubham0x13@users.noreply.github.com>
2025-06-05 11:23:46 +0530

    Simplify grid bounds unpacking

--- adafruit_displayio_layout/layouts/grid_layout.py
+++ adafruit_displayio_layout/layouts/grid_layout.py
@@ -378,10 +378,8 @@ class GridLayout(displayio.Group):
          then the cell_anchor_point from the GridLayout will be used.
         :return: None"""
 
-        grid_x = grid_position[0]
-        grid_y = grid_position[1]
-        max_grid_x = self.grid_size[0]
-        max_grid_y = self.grid_size[1]
+        grid_x, grid_y = grid_position
+        max_grid_x, max_grid_y = self.grid_size
         if grid_x >= max_grid_x or grid_y >= max_grid_y:
             raise ValueError(
                 f"Grid position {grid_position} is out of bounds for grid size {self.grid_size}"
